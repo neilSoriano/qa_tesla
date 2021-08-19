6050def2191393a61f2c9a141ac6f8ac84a0868f
Neil Soriano <neilsoriano@ymail.com>
2021-08-18 18:57:23 -0700

    Added UI automation to customizing Model X

--- __tests__/customize.test.ts
+++ __tests__/customize.test.ts
@@ -1,5 +1,5 @@
 import { Tesla } from "../src/Tesla";
-import { WebDriver, Builder, Capabilities, By, until } from "selenium-webdriver";
+import { WebDriver, Builder, Capabilities, By, until, Key } from "selenium-webdriver";
 
 const chromedriver = require("chromedriver");
 
@@ -8,3 +8,55 @@ const driver: WebDriver = new Builder()
   .build();
 
 const page = new Tesla(driver);
+const actions = driver.actions();
+
+test("Tesla Model X Customization", async () => {
+  await page.navigate();
+
+  await page.click(page.modelXButton);
+  await driver.sleep(2000);
+
+  await page.click(page.orderNowButton);
+  await driver.sleep(2000);
+
+  await page.click(page.plaid);
+  await driver.sleep(2000);
+
+  await actions.sendKeys(Key.PAGE_DOWN).perform();
+  await driver.sleep(1000);
+
+  await page.click(page.solidBlack);
+  await driver.sleep(2000);
+
+  await page.click(page.gallery);
+
+  await actions
+  .keyDown(Key.ARROW_DOWN)
+  .pause()
+  .pause()
+  .pause()
+  .keyUp(Key.ARROW_DOWN)
+  .perform();
+  await driver.sleep(1000);
+
+  await page.click(page.turbineWheels);
+  await driver.sleep(2000);
+
+  await page.click(page.whiteInterior);
+  await driver.sleep(2000);
+
+  await page.click(page.sevenSeats);
+  await driver.sleep(2000);
+
+  await actions.sendKeys(Key.PAGE_DOWN).perform();
+  await actions.sendKeys(Key.PAGE_DOWN).perform();
+  await driver.sleep(1000);
+
+  await page.click(page.selfDriving);
+  await driver.sleep(2000);
+
+})
+
+// afterAll(async () => {
+//   await driver.quit();
+// });
--- src/Tesla.ts
+++ src/Tesla.ts
@@ -1,8 +1,43 @@
-import { By, until, WebDriver } from "selenium-webdriver";
+import { Actions, By, Key, until, WebDriver } from "selenium-webdriver";
 
 export class Tesla {
     driver: WebDriver;
     url: string = "https://www.tesla.com/"
+
+    // locator for Tesla logo (home button)
+    teslaLogo: By = By.xpath("//a[@aria-label='Tesla Logo']");
+
+    // locator for menu button
+    // menuButton: By = By.xpath("//button[@class='tds-site-nav-item tds--highlighted tds-site-header-menu-link']");
+
+    //locator for model x button in drop down menu
+    modelXButton: By = By.xpath("//a[@href='/modelx']");
+
+    // locator for order now button on model x page
+    orderNowButton: By = By.xpath("(//a[@data-gtm-interaction='order now'])[1]");
+
+    // locator for gallery
+    gallery: By = By.className("cf-asset-wrapper");
+
+    // locator for plaid option button
+    plaid: By = By.xpath("//label[@for='$MTX11-Plaid']");
+
+    // locator for black paint option
+    solidBlack: By = By.css("label[for='PAINT_$PBSB']");
+
+    // locator for turbine wheels
+    turbineWheels: By = By.xpath("//label[@for='WHEELS_$WX20']");
+
+    // locator for black and white interior
+    whiteInterior: By = By.css("label[for='INTERIOR_PACKAGE_$IWC00']");
+
+    // locator for seven seats
+    sevenSeats: By = By.xpath('//div[@aria-label="Seven Seats"]');
+    
+    // locator for add button for self driving capability
+    selfDriving: By = By.xpath('//button[@aria-label="Select Option - Full Self-Driving Capability - $10,000"]');
+
+
 
     constructor(driver: WebDriver) {
         this.driver = driver;
@@ -10,6 +45,8 @@ export class Tesla {
 
     async navigate() {
         await this.driver.get(this.url);
+        await this.driver.wait(until.elementLocated(this.teslaLogo));
+        await this.driver.wait(until.elementIsVisible(await this.driver.findElement(this.teslaLogo)));
     }
 
     async sendKeys(elementBy: By, keys) {
